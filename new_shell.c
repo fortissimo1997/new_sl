1b1997bf94bda9b818a8c02ab35c37785c99a3f6
fortissimo1997 <fortissimo1997@gmail.com>
2011-12-21 14:50:04 +0900

    fork/execに変更。動かず

--- new_shell.c
+++ new_shell.c
@@ -8,6 +8,11 @@
 
 int add_line(int x, int h, char *buffer);
 
+void sigcatch(int sig){
+  printf("exit\n");
+  exit(0);
+}
+
 int my_mvaddstr(int y, int x, char *str){
   char *p = str;
   for(; x < 0; ++x, ++p)
@@ -22,45 +27,73 @@ int main(int argc, char *argv[]){
   int h = 0;
   char *buffer;
   char *shell, *eob;
-  FILE *fp;
-  if((buffer = malloc(4096 * sizeof(char))) == NULL){
-    fprintf(stderr, "cannot allocate buffer!\n");
-    exit(-1);
-  }
+  pid_t cpid;
+  int pipefd[2];
+
+
   shell = getenv("SHELL");
   if(shell == NULL){
     fprintf(stderr, "cannot get login shell!\n");
-    exit(-1);
+    exit(EXIT_FAILURE);
   }
-  if((fp = popen(shell, "r")) == NULL){
-    fprintf(stderr, "cannot open shell!\n");
-    exit(-1);
+
+  if(pipe(pipefd) == -1){
+    perror("pipe");
+    exit(EXIT_FAILURE);
   }
-  initscr();
-  noecho();
-  leaveok(stdscr, TRUE);
-  scrollok(stdscr, FALSE);
-  while(1){
-    eob = fgets(buffer, 4096, fp);
-    if(feof(fp)) break;
-    /*
-    eob = strchr(buffer, '\n');
-    if(eob != NULL)
-      *eob = '\0';
-      */
-    for(x = COLS - 1; ; --x){
-      if(add_line(x, h, buffer) == ERR) break;
-      refresh();
-      usleep(20000);
+
+  cpid = fork();
+  if(cpid == -1){
+    close(pipefd[0]);
+    close(pipefd[1]);
+    perror("fork");
+    exit(EXIT_FAILURE);
+  }
+
+  if(cpid == 0){
+    close(pipefd[0]);
+    dup2(pipefd[1], 1);
+    close(pipefd[1]);
+
+    execl(shell, shell);
+    exit(EXIT_FAILURE);
+  }else{
+    close(pipefd[1]);
+    dup2(pipefd[0], 0);
+    close(pipefd[0]);
+
+    if(SIG_ERR == signal(SIGCHLD, sigcatch)){
+      perror("signal");
+      exit(1);
     }
-    mvcur(0, COLS - 1, LINES - 1, 0);
-    h++;
+    
+    if((buffer = malloc(4096 * sizeof(char))) == NULL){
+      fprintf(stderr, "cannot allocate buffer!\n");
+      exit(EXIT_FAILURE);
+    }
+
+    initscr();
+    noecho();
+    leaveok(stdscr, TRUE);
+    scrollok(stdscr, FALSE);
+    while(1){
+      eob = fgets(buffer, 4096, stdin);
+      eob = strchr(buffer, '\n');
+      if(eob != NULL)
+        *eob = '\0';
+      for(x = COLS - 1; ; --x){
+        if(add_line(x, h, buffer) == ERR) break;
+        refresh();
+        usleep(20000);
+      }
+      mvcur(0, COLS - 1, LINES - 1, 0);
+      h++;
     
 //    fprintf(stdout, "%s\n", buffer);
+    }
   }
 
   endwin();
-  pclose(fp);
   free(buffer);
   return 0;
 }
